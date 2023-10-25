fa52c441fb13dc98375c38382be3c26ac35d5c8d
Alexander Filimonov <a.filimonov@prequelapp.com>
2023-10-25 09:51:29 +0100

    initial commit

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "PizzaOnboarding",
+    platforms: [
+        .iOS(.v15),
+        .tvOS(.v15), 
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(
+            name: "PizzaOnboarding",
+            targets: ["PizzaOnboarding"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PizzaKit/pizzakit",
+            from: "1.0.1"
+        )
+    ],
+    targets: [
+        .target(
+            name: "PizzaOnboarding",
+            dependencies: [
+                .product(name: "PizzaKit", package: "pizzakit")
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
